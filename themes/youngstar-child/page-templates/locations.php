73c2e40d84fa2d022b6f21f5d3139cebccacb3b4
acelero-webdev <webdev@acelero.net>
2025-03-19 14:17:01 -0500

    remove the hard coded footer on the locations page

--- themes/youngstar-child/page-templates/locations.php
+++ themes/youngstar-child/page-templates/locations.php
@@ -16,23 +16,4 @@
 
 <?php the_content(); ?>
 
-
-<div class="container-full homepage_bottom">
-    <div class="container">
-        <div class="row row-45-55">
-            <div class="p-0-10">
-                <h2>Share Your Voice!</h2>
-<p>YoungStar Connect wants to ensure enrolling and participating in YoungStar is the best experience it possibly can be. Share your ideas and suggestions by emailing us at <a href="mailto:info@youngstarconnect.com">info@youngstarconnect.com</a>.</p>
-            </div>
-            <div>
-                <div class="homepage_bottom_grid p-0-10">
-                    <div class="row">
-
-                                            </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
 <?php get_footer(); ?>
